2df007bfd6cdaa86dd43713270886faead58b0d5
buildbot <glarsen@aquaveo.com>
2018-07-16 10:34:50 -0600

    add enums to pybindings

--- xmsgrid/python/ugrid/XmUGrid_py.cpp
+++ xmsgrid/python/ugrid/XmUGrid_py.cpp
@@ -19,7 +19,71 @@ namespace py = pybind11;
 PYBIND11_DECLARE_HOLDER_TYPE(T, boost::shared_ptr<T>);
 
 void initXmUGrid(py::module &m) {
-    // py::class_<xms::XmUGrid, boost::shared_ptr<xms::XmUGrid>>(m, "XmUGrid")
+    py::class_<xms::XmUGrid, boost::shared_ptr<xms::XmUGrid>> xmUgrid(m, "XmUGrid");
     //     .def(py::init(&xms::XmUGrid::New))
-    //     ;
+
+    // UGrid CellType
+    py::enum_<xms::XmUGridCellType>(xmUgrid, "xmugrid_celltype_enum",
+                                    "ugrid_celltype_enum for XmUGrid class")
+        .value("XMU_INVALID_CELL_TYPE", xms::XmUGridCellType::XMU_INVALID_CELL_TYPE)
+        .value("XMU_EMPTY_CELL", xms::XmUGridCellType::XMU_EMPTY_CELL)
+        .value("XMU_VERTEX", xms::XmUGridCellType::XMU_VERTEX)
+        .value("XMU_POLY_VERTEX", xms::XmUGridCellType::XMU_POLY_VERTEX)
+        .value("XMU_LINE", xms::XmUGridCellType::XMU_LINE)
+        .value("XMU_POLY_LINE", xms::XmUGridCellType::XMU_POLY_LINE)
+        .value("XMU_TRIANGLE", xms::XmUGridCellType::XMU_TRIANGLE)
+        .value("XMU_TRIANGLE_STRIP", xms::XmUGridCellType::XMU_TRIANGLE_STRIP)
+        .value("XMU_POLYGON", xms::XmUGridCellType::XMU_POLYGON)
+        .value("XMU_PIXEL", xms::XmUGridCellType::XMU_PIXEL)
+        .value("XMU_QUAD", xms::XmUGridCellType::XMU_QUAD)
+        .value("XMU_TETRA", xms::XmUGridCellType::XMU_TETRA)
+        .value("XMU_VOXEL", xms::XmUGridCellType::XMU_VOXEL)
+        .value("XMU_HEXAHEDRON", xms::XmUGridCellType::XMU_HEXAHEDRON)
+        .value("XMU_WEDGE", xms::XmUGridCellType::XMU_WEDGE)
+        .value("XMU_PYRAMID", xms::XmUGridCellType::XMU_PYRAMID)
+        .value("XMU_PENTAGONAL_PRISM", xms::XmUGridCellType::XMU_PENTAGONAL_PRISM)
+        .value("XMU_HEXAGONAL_PRISM", xms::XmUGridCellType::XMU_HEXAGONAL_PRISM)
+        .value("XMU_QUADRATIC_EDGE", xms::XmUGridCellType::XMU_QUADRATIC_EDGE)
+        .value("XMU_QUADRATIC_TRIANGLE", xms::XmUGridCellType::XMU_QUADRATIC_TRIANGLE)
+        .value("XMU_QUADRATIC_QUAD", xms::XmUGridCellType::XMU_QUADRATIC_QUAD)
+        .value("XMU_QUADRATIC_POLYGON", xms::XmUGridCellType::XMU_QUADRATIC_POLYGON)
+        .value("XMU_QUADRATIC_TETRA", xms::XmUGridCellType::XMU_QUADRATIC_TETRA)
+        .value("XMU_QUADRATIC_HEXAHEDRON", xms::XmUGridCellType::XMU_QUADRATIC_HEXAHEDRON)
+        .value("XMU_QUADRATIC_WEDGE", xms::XmUGridCellType::XMU_QUADRATIC_WEDGE)
+        .value("XMU_QUADRATIC_PYRAMID", xms::XmUGridCellType::XMU_QUADRATIC_PYRAMID)
+        .value("XMU_BIQUADRATIC_QUAD", xms::XmUGridCellType::XMU_BIQUADRATIC_QUAD)
+        .value("XMU_TRIQUADRATIC_HEXAHEDRON", xms::XmUGridCellType::XMU_TRIQUADRATIC_HEXAHEDRON)
+        .value("XMU_QUADRATIC_LINEAR_QUAD", xms::XmUGridCellType::XMU_QUADRATIC_LINEAR_QUAD)
+        .value("XMU_QUADRATIC_LINEAR_WEDGE", xms::XmUGridCellType::XMU_QUADRATIC_LINEAR_WEDGE)
+        .value("XMU_BIQUADRATIC_QUADRATIC_WEDGE", xms::XmUGridCellType::XMU_BIQUADRATIC_QUADRATIC_WEDGE)
+        .value("XMU_BIQUADRATIC_QUADRATIC_HEXAHEDRON", xms::XmUGridCellType::XMU_BIQUADRATIC_QUADRATIC_HEXAHEDRON)
+        .value("XMU_BIQUADRATIC_TRIANGLE", xms::XmUGridCellType::XMU_BIQUADRATIC_TRIANGLE)
+        .value("XMU_CUBIC_LINE", xms::XmUGridCellType::XMU_CUBIC_LINE)
+        .value("XMU_CONVEX_POINT_SET", xms::XmUGridCellType::XMU_CONVEX_POINT_SET)
+        .value("XMU_POLYHEDRON", xms::XmUGridCellType::XMU_POLYHEDRON)
+        .value("XMU_PARAMETRIC_CURVE", xms::XmUGridCellType::XMU_PARAMETRIC_CURVE)
+        .value("XMU_PARAMETRIC_SURFACE", xms::XmUGridCellType::XMU_PARAMETRIC_SURFACE)
+        .value("XMU_PARAMETRIC_TRI_SURFACE", xms::XmUGridCellType::XMU_PARAMETRIC_TRI_SURFACE)
+        .value("XMU_PARAMETRIC_QUAD_SURFACE", xms::XmUGridCellType::XMU_PARAMETRIC_QUAD_SURFACE)
+        .value("XMU_PARAMETRIC_TETRA_REGION", xms::XmUGridCellType::XMU_PARAMETRIC_TETRA_REGION)
+        .value("XMU_PARAMETRIC_HEX_REGION", xms::XmUGridCellType::XMU_PARAMETRIC_HEX_REGION)
+        .value("XMU_HIGHER_ORDER_EDGE", xms::XmUGridCellType::XMU_HIGHER_ORDER_EDGE)
+        .value("XMU_HIGHER_ORDER_TRIANGLE", xms::XmUGridCellType::XMU_HIGHER_ORDER_TRIANGLE)
+        .value("XMU_HIGHER_ORDER_QUAD", xms::XmUGridCellType::XMU_HIGHER_ORDER_QUAD)
+        .value("XMU_HIGHER_ORDER_POLYGON", xms::XmUGridCellType::XMU_HIGHER_ORDER_POLYGON)
+        .value("XMU_HIGHER_ORDER_TETRAHEDRON", xms::XmUGridCellType::XMU_HIGHER_ORDER_TETRAHEDRON)
+        .value("XMU_HIGHER_ORDER_WEDGE", xms::XmUGridCellType::XMU_HIGHER_ORDER_WEDGE)
+        .value("XMU_HIGHER_ORDER_PYRAMID", xms::XmUGridCellType::XMU_HIGHER_ORDER_PYRAMID)
+        .value("XMU_HIGHER_ORDER_HEXAHEDRON", xms::XmUGridCellType::XMU_HIGHER_ORDER_HEXAHEDRON)
+        .value("XMU_NUMBER_OF_CELL_TYPES", xms::XmUGridCellType::XMU_NUMBER_OF_CELL_TYPES)
+        .export_values();
+
+    // UGrid FaceOrientation
+    py::enum_<xms::XmUGridFaceOrientation>(xmUgrid, "xmugrid_faceorientation_enum",
+                                    "xmugrid_faceorientation_enum for XmUGrid class")
+        .value("XMU_ORIENTATION_UNKNOWN", xms::XmUGridFaceOrientation::XMU_ORIENTATION_UNKNOWN)
+        .value("XMU_ORIENTATION_SIDE", xms::XmUGridFaceOrientation::XMU_ORIENTATION_SIDE)
+        .value("XMU_ORIENTATION_TOP", xms::XmUGridFaceOrientation::XMU_ORIENTATION_TOP)
+        .value("XMU_ORIENTATION_BOTTOM", xms::XmUGridFaceOrientation::XMU_ORIENTATION_BOTTOM)
+        .export_values();
 }
--- xmsgrid/python/ugrid/XmUGrid_pyt.py
+++ xmsgrid/python/ugrid/XmUGrid_pyt.py
@@ -1,1 +1,112 @@
-# Place holder for XMUGrid Tests
+"""Test XmUGrid_py.cpp"""
+import unittest
+import xmsgrid_py
+
+
+class TestXmUGridCellTypeEnum(unittest.TestCase):
+    """CellType enum tests"""
+    def test_xmugrid_celltype_enum(self):
+        from xmsgrid_py.ugrid import XmUGrid
+        self.assertEqual("xmugrid_celltype_enum.XMU_INVALID_CELL_TYPE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_INVALID_CELL_TYPE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_EMPTY_CELL", str(XmUGrid.xmugrid_celltype_enum.XMU_EMPTY_CELL))
+        self.assertEqual("xmugrid_celltype_enum.XMU_VERTEX", str(XmUGrid.xmugrid_celltype_enum.XMU_VERTEX))
+        self.assertEqual("xmugrid_celltype_enum.XMU_POLY_VERTEX", str(XmUGrid.xmugrid_celltype_enum.XMU_POLY_VERTEX))
+        self.assertEqual("xmugrid_celltype_enum.XMU_LINE", str(XmUGrid.xmugrid_celltype_enum.XMU_LINE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_POLY_LINE", str(XmUGrid.xmugrid_celltype_enum.XMU_POLY_LINE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_TRIANGLE", str(XmUGrid.xmugrid_celltype_enum.XMU_TRIANGLE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_TRIANGLE_STRIP",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_TRIANGLE_STRIP))
+        self.assertEqual("xmugrid_celltype_enum.XMU_POLYGON", str(XmUGrid.xmugrid_celltype_enum.XMU_POLYGON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PIXEL", str(XmUGrid.xmugrid_celltype_enum.XMU_PIXEL))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUAD", str(XmUGrid.xmugrid_celltype_enum.XMU_QUAD))
+        self.assertEqual("xmugrid_celltype_enum.XMU_TETRA", str(XmUGrid.xmugrid_celltype_enum.XMU_TETRA))
+        self.assertEqual("xmugrid_celltype_enum.XMU_VOXEL", str(XmUGrid.xmugrid_celltype_enum.XMU_VOXEL))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HEXAHEDRON", str(XmUGrid.xmugrid_celltype_enum.XMU_HEXAHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_WEDGE", str(XmUGrid.xmugrid_celltype_enum.XMU_WEDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PYRAMID", str(XmUGrid.xmugrid_celltype_enum.XMU_PYRAMID))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PENTAGONAL_PRISM",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_PENTAGONAL_PRISM))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HEXAGONAL_PRISM",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_HEXAGONAL_PRISM))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_EDGE",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_EDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_TRIANGLE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_TRIANGLE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_QUAD",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_QUAD))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_POLYGON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_POLYGON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_TETRA",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_TETRA))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_HEXAHEDRON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_HEXAHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_WEDGE",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_WEDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_PYRAMID",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_PYRAMID))
+        self.assertEqual("xmugrid_celltype_enum.XMU_BIQUADRATIC_QUAD",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_BIQUADRATIC_QUAD))
+        self.assertEqual("xmugrid_celltype_enum.XMU_TRIQUADRATIC_HEXAHEDRON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_TRIQUADRATIC_HEXAHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_LINEAR_QUAD",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_LINEAR_QUAD))
+        self.assertEqual("xmugrid_celltype_enum.XMU_QUADRATIC_LINEAR_WEDGE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_QUADRATIC_LINEAR_WEDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_BIQUADRATIC_QUADRATIC_WEDGE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_BIQUADRATIC_QUADRATIC_WEDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_BIQUADRATIC_QUADRATIC_HEXAHEDRON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_BIQUADRATIC_QUADRATIC_HEXAHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_BIQUADRATIC_TRIANGLE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_BIQUADRATIC_TRIANGLE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_CUBIC_LINE", str(XmUGrid.xmugrid_celltype_enum.XMU_CUBIC_LINE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_CONVEX_POINT_SET",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_CONVEX_POINT_SET))
+        self.assertEqual("xmugrid_celltype_enum.XMU_POLYHEDRON",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_POLYHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PARAMETRIC_CURVE",
+                         str(XmUGrid.xmugrid_celltype_enum.XMU_PARAMETRIC_CURVE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PARAMETRIC_SURFACE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_PARAMETRIC_SURFACE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PARAMETRIC_TRI_SURFACE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_PARAMETRIC_TRI_SURFACE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PARAMETRIC_QUAD_SURFACE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_PARAMETRIC_QUAD_SURFACE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PARAMETRIC_TETRA_REGION",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_PARAMETRIC_TETRA_REGION))
+        self.assertEqual("xmugrid_celltype_enum.XMU_PARAMETRIC_HEX_REGION",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_PARAMETRIC_HEX_REGION))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_EDGE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_EDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_TRIANGLE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_TRIANGLE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_QUAD",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_QUAD))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_POLYGON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_POLYGON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_TETRAHEDRON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_TETRAHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_WEDGE",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_WEDGE))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_PYRAMID",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_PYRAMID))
+        self.assertEqual("xmugrid_celltype_enum.XMU_HIGHER_ORDER_HEXAHEDRON",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_HIGHER_ORDER_HEXAHEDRON))
+        self.assertEqual("xmugrid_celltype_enum.XMU_NUMBER_OF_CELL_TYPES",
+                          str(XmUGrid.xmugrid_celltype_enum.XMU_NUMBER_OF_CELL_TYPES))
+        self.assertEqual(51, len(XmUGrid.xmugrid_celltype_enum.__members__))
+
+
+class TestXmUGridFaceOrientationEnum(unittest.TestCase):
+    """FaceOrientation enum tests"""
+    def test_xmugrid_faceorientation_enum(self):
+        from xmsgrid_py.ugrid import XmUGrid
+        self.assertEqual("xmugrid_faceorientation_enum.XMU_ORIENTATION_UNKNOWN",
+                          str(XmUGrid.xmugrid_faceorientation_enum.XMU_ORIENTATION_UNKNOWN))
+        self.assertEqual("xmugrid_faceorientation_enum.XMU_ORIENTATION_SIDE",
+                          str(XmUGrid.xmugrid_faceorientation_enum.XMU_ORIENTATION_SIDE))
+        self.assertEqual("xmugrid_faceorientation_enum.XMU_ORIENTATION_TOP",
+                          str(XmUGrid.xmugrid_faceorientation_enum.XMU_ORIENTATION_TOP))
+        self.assertEqual("xmugrid_faceorientation_enum.XMU_ORIENTATION_BOTTOM",
+                          str(XmUGrid.xmugrid_faceorientation_enum.XMU_ORIENTATION_BOTTOM))
+        self.assertEqual(4, len(XmUGrid.xmugrid_faceorientation_enum.__members__))
