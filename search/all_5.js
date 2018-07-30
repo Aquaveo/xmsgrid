31cb7760ebf6196a34a8892e60622df1a06a83cc
Travis CI <travis@travis-ci.org>
2018-07-30 17:38:22 +0000

    Deploy code docs to GitHub Pages Travis build: 17
    Commit: 64865c998c15c6f903080aa4b14f48a4bd6b1dfc

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['new',['New',['../classxms_1_1_xm_u_grid.html#ad95e11b54ca04baa73571cc4f939a27d',1,'xms::XmUGrid::New(const VecPt3d &amp;a_points, const VecInt &amp;a_cellStream)'],['../classxms_1_1_xm_u_grid.html#a51464b4cc71bddbb97e9e8a6ff7fb798',1,'xms::XmUGrid::New()']]]
+  ['iscellvalidwithpointchange',['IsCellValidWithPointChange',['../classxms_1_1_xm_u_grid_impl.html#ac199ae8bef7c8fc4b0c840ba6428dbf5',1,'xms::XmUGridImpl']]],
+  ['isfaceside',['IsFaceSide',['../classxms_1_1_xm_u_grid_impl.html#af58fa7a901ed95cd28b67417bb511ee8',1,'xms::XmUGridImpl']]]
 ];
