4f0c626b521f2db14489ec53fd21c440cf1d2dec
claytonk2 <ckingsbury2@gmail.com>
2018-10-23 11:54:45 -0600

    a

--- xmsgrid/python/xmsgrid_py.cpp
+++ xmsgrid/python/xmsgrid_py.cpp
@@ -24,11 +24,9 @@ PYBIND11_MODULE(xmsgrid_py, m) {
     m.def("version", &version,
           "Get current version of xmsgrid Python bindings.");
     const char* misc_doc = R"pydoc(
-        The misc module of the xmsgrid python library contains classes and
-        functions that are shared between all of the xms family of libraries.
-        These functions and classes can be used in any of the library to ensure
-        that functionality is standardized. Xmsgrid is a grid geometry library 
-        used in xms libraries and products.
+        Xmsgrid is a grid geometry library used in xms libraries and products. 
+        The library supports unstructured grids with 0D, 1D, 2D and 3D elements. 
+        Contains functions to read and write unstructered grids.
     )pydoc"; 
     // UGrid module
     py::module modUGrid = m.def_submodule("ugrid", misc_doc);
