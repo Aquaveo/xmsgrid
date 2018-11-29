fc2aa220705912c2718a3bee58b2413e2139177b
Bill Dolinar <bdolinar@aquaveo.com>
2018-11-29 05:42:51 -0700

    Fixed a few doxygen comments.

--- xmsgrid/python/ugrid/XmUGridUtils_py.cpp
+++ xmsgrid/python/ugrid/XmUGridUtils_py.cpp
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 /// \file
-/// \brief
+/// \brief Python bindings for XmUGridUtils.h.
 /// \copyright (C) Copyright Aquaveo 2018. Distributed under FreeBSD License
 /// (See accompanying file LICENSE or https://aqaveo.com/bsd/license.txt)
 //------------------------------------------------------------------------------
