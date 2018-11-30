919d1856df755f28f9e87600afe4aec3448d61a4
alemon <alemon@aquaveo.com>
2018-11-30 14:37:21 -0700

    remove xm from enums and point xmsgrid to correct version of core

--- xmsgrid/python/ugrid/XmEdge_pyt.py
+++ xmsgrid/python/ugrid/XmEdge_pyt.py
@@ -1,4 +1,4 @@
-# Place holder for XMugrid_utils Tests
+# Place holder for ugrid_utils Tests
 import unittest
 from xmsgrid.ugrid import ugrid_utils
 from xmsgrid.ugrid import UGrid
@@ -16,19 +16,19 @@ class TestUGridUtilFunctions(unittest.TestCase):
                   10), (0, 10, 10), (10, 10, 10), (20, 10, 10), (30, 10, 10), 
                   (40, 10, 10), (0, 20, 10), (10, 20, 10), (20, 20, 10), (30, 20
                   , 10), (40, 20, 10));
-        cells = (UGrid.xmugrid_celltype_enum.XMU_TETRA, 4, 0, 1, 5, 15,
-                 UGrid.xmugrid_celltype_enum.XMU_VOXEL, 8, 1, 2, 6, 7, 16, 17,
-                 21, 22, UGrid.xmugrid_celltype_enum.XMU_HEXAHEDRON, 8, 2, 3,
+        cells = (UGrid.ugrid_celltype_enum.TETRA, 4, 0, 1, 5, 15,
+                 UGrid.ugrid_celltype_enum.VOXEL, 8, 1, 2, 6, 7, 16, 17,
+                 21, 22, UGrid.ugrid_celltype_enum.HEXAHEDRON, 8, 2, 3,
                  8, 7, 17, 18, 23, 22, 
-                 UGrid.xmugrid_celltype_enum.XMU_POLYHEDRON, 6,
+                 UGrid.ugrid_celltype_enum.POLYHEDRON, 6,
                  4, 9, 8, 13, 14, # Bottom face with 4 points : 9, 8, 13, 14
                  4, 8, 9, 24, 23, # Front face with 4 points : 8, 9, 24, 23
                  4, 9, 14, 29, 24, # Right face with 4 points : 9, 14, 29, 28
                  4, 14, 13, 28, 29, # Back face with 4 points : 14, 13, 28, 29
                  4, 8, 13, 28, 23, # Left face with 4 points : 13, 8, 23, 28
                  4, 23, 24, 29, 28, # Top face with 4 points : 23, 24, 29, 28
-                 UGrid.xmugrid_celltype_enum.XMU_WEDGE, 6, 3, 4, 18, 8, 9, 23,
-                 UGrid.xmugrid_celltype_enum.XMU_PYRAMID, 5, 5, 6, 11, 10, 20)
+                 UGrid.ugrid_celltype_enum.WEDGE, 6, 3, 4, 18, 8, 9, 23,
+                 UGrid.ugrid_celltype_enum.PYRAMID, 5, 5, 6, 11, 10, 20)
         xu = UGrid(points, cells)
         return xu
 
--- xmsgrid/python/ugrid/XmUGrid_py.cpp
+++ xmsgrid/python/ugrid/XmUGrid_py.cpp
@@ -754,100 +754,100 @@ void initXmUGrid(py::module &m) {
              get_cell3d_face_orientation_doc, py::arg("cell_idx"), py::arg("face_idx"));
 
     // UGrid CellType
-    py::enum_<xms::XmUGridCellType>(xmUg, "xmugrid_celltype_enum",
-                                    "ugrid_celltype_enum for XmUGrid class")
-        .value("XMU_INVALID_CELL_TYPE", 
+    py::enum_<xms::XmUGridCellType>(xmUg, "ugrid_celltype_enum",
+                                    "ugrid_celltype_enum for UGrid class")
+        .value("INVALID_CELL_TYPE",
             xms::XmUGridCellType::XMU_INVALID_CELL_TYPE)
-        .value("XMU_EMPTY_CELL", xms::XmUGridCellType::XMU_EMPTY_CELL)
-        .value("XMU_VERTEX", xms::XmUGridCellType::XMU_VERTEX)
-        .value("XMU_POLY_VERTEX", xms::XmUGridCellType::XMU_POLY_VERTEX)
-        .value("XMU_LINE", xms::XmUGridCellType::XMU_LINE)
-        .value("XMU_POLY_LINE", xms::XmUGridCellType::XMU_POLY_LINE)
-        .value("XMU_TRIANGLE", xms::XmUGridCellType::XMU_TRIANGLE)
-        .value("XMU_TRIANGLE_STRIP", xms::XmUGridCellType::XMU_TRIANGLE_STRIP)
-        .value("XMU_POLYGON", xms::XmUGridCellType::XMU_POLYGON)
-        .value("XMU_PIXEL", xms::XmUGridCellType::XMU_PIXEL)
-        .value("XMU_QUAD", xms::XmUGridCellType::XMU_QUAD)
-        .value("XMU_TETRA", xms::XmUGridCellType::XMU_TETRA)
-        .value("XMU_VOXEL", xms::XmUGridCellType::XMU_VOXEL)
-        .value("XMU_HEXAHEDRON", xms::XmUGridCellType::XMU_HEXAHEDRON)
-        .value("XMU_WEDGE", xms::XmUGridCellType::XMU_WEDGE)
-        .value("XMU_PYRAMID", xms::XmUGridCellType::XMU_PYRAMID)
-        .value("XMU_PENTAGONAL_PRISM", 
+        .value("EMPTY_CELL", xms::XmUGridCellType::XMU_EMPTY_CELL)
+        .value("VERTEX", xms::XmUGridCellType::XMU_VERTEX)
+        .value("POLY_VERTEX", xms::XmUGridCellType::XMU_POLY_VERTEX)
+        .value("LINE", xms::XmUGridCellType::XMU_LINE)
+        .value("POLY_LINE", xms::XmUGridCellType::XMU_POLY_LINE)
+        .value("TRIANGLE", xms::XmUGridCellType::XMU_TRIANGLE)
+        .value("TRIANGLE_STRIP", xms::XmUGridCellType::XMU_TRIANGLE_STRIP)
+        .value("POLYGON", xms::XmUGridCellType::XMU_POLYGON)
+        .value("PIXEL", xms::XmUGridCellType::XMU_PIXEL)
+        .value("QUAD", xms::XmUGridCellType::XMU_QUAD)
+        .value("TETRA", xms::XmUGridCellType::XMU_TETRA)
+        .value("VOXEL", xms::XmUGridCellType::XMU_VOXEL)
+        .value("HEXAHEDRON", xms::XmUGridCellType::XMU_HEXAHEDRON)
+        .value("WEDGE", xms::XmUGridCellType::XMU_WEDGE)
+        .value("PYRAMID", xms::XmUGridCellType::XMU_PYRAMID)
+        .value("PENTAGONAL_PRISM",
             xms::XmUGridCellType::XMU_PENTAGONAL_PRISM)
-        .value("XMU_HEXAGONAL_PRISM", xms::XmUGridCellType::XMU_HEXAGONAL_PRISM)
-        .value("XMU_QUADRATIC_EDGE", xms::XmUGridCellType::XMU_QUADRATIC_EDGE)
-        .value("XMU_QUADRATIC_TRIANGLE", 
+        .value("HEXAGONAL_PRISM", xms::XmUGridCellType::XMU_HEXAGONAL_PRISM)
+        .value("QUADRATIC_EDGE", xms::XmUGridCellType::XMU_QUADRATIC_EDGE)
+        .value("QUADRATIC_TRIANGLE",
             xms::XmUGridCellType::XMU_QUADRATIC_TRIANGLE)
-        .value("XMU_QUADRATIC_QUAD", xms::XmUGridCellType::XMU_QUADRATIC_QUAD)
-        .value("XMU_QUADRATIC_POLYGON", 
+        .value("QUADRATIC_QUAD", xms::XmUGridCellType::XMU_QUADRATIC_QUAD)
+        .value("QUADRATIC_POLYGON",
             xms::XmUGridCellType::XMU_QUADRATIC_POLYGON)
-        .value("XMU_QUADRATIC_TETRA", xms::XmUGridCellType::XMU_QUADRATIC_TETRA)
-        .value("XMU_QUADRATIC_HEXAHEDRON", 
+        .value("QUADRATIC_TETRA", xms::XmUGridCellType::XMU_QUADRATIC_TETRA)
+        .value("QUADRATIC_HEXAHEDRON",
             xms::XmUGridCellType::XMU_QUADRATIC_HEXAHEDRON)
-        .value("XMU_QUADRATIC_WEDGE", xms::XmUGridCellType::XMU_QUADRATIC_WEDGE)
-        .value("XMU_QUADRATIC_PYRAMID", 
+        .value("QUADRATIC_WEDGE", xms::XmUGridCellType::XMU_QUADRATIC_WEDGE)
+        .value("QUADRATIC_PYRAMID",
             xms::XmUGridCellType::XMU_QUADRATIC_PYRAMID)
-        .value("XMU_BIQUADRATIC_QUAD", 
+        .value("BIQUADRATIC_QUAD",
             xms::XmUGridCellType::XMU_BIQUADRATIC_QUAD)
-        .value("XMU_TRIQUADRATIC_HEXAHEDRON", 
+        .value("TRIQUADRATIC_HEXAHEDRON",
             xms::XmUGridCellType::XMU_TRIQUADRATIC_HEXAHEDRON)
-        .value("XMU_QUADRATIC_LINEAR_QUAD", 
+        .value("QUADRATIC_LINEAR_QUAD",
             xms::XmUGridCellType::XMU_QUADRATIC_LINEAR_QUAD)
-        .value("XMU_QUADRATIC_LINEAR_WEDGE", 
+        .value("QUADRATIC_LINEAR_WEDGE",
             xms::XmUGridCellType::XMU_QUADRATIC_LINEAR_WEDGE)
-        .value("XMU_BIQUADRATIC_QUADRATIC_WEDGE", 
+        .value("BIQUADRATIC_QUADRATIC_WEDGE",
             xms::XmUGridCellType::XMU_BIQUADRATIC_QUADRATIC_WEDGE)
-        .value("XMU_BIQUADRATIC_QUADRATIC_HEXAHEDRON", 
+        .value("BIQUADRATIC_QUADRATIC_HEXAHEDRON",
             xms::XmUGridCellType::XMU_BIQUADRATIC_QUADRATIC_HEXAHEDRON)
-        .value("XMU_BIQUADRATIC_TRIANGLE", 
+        .value("BIQUADRATIC_TRIANGLE",
             xms::XmUGridCellType::XMU_BIQUADRATIC_TRIANGLE)
-        .value("XMU_CUBIC_LINE", xms::XmUGridCellType::XMU_CUBIC_LINE)
-        .value("XMU_CONVEX_POINT_SET", 
+        .value("CUBIC_LINE", xms::XmUGridCellType::XMU_CUBIC_LINE)
+        .value("CONVEX_POINT_SET",
             xms::XmUGridCellType::XMU_CONVEX_POINT_SET)
-        .value("XMU_POLYHEDRON", xms::XmUGridCellType::XMU_POLYHEDRON)
-        .value("XMU_PARAMETRIC_CURVE", 
+        .value("POLYHEDRON", xms::XmUGridCellType::XMU_POLYHEDRON)
+        .value("PARAMETRIC_CURVE",
             xms::XmUGridCellType::XMU_PARAMETRIC_CURVE)
-        .value("XMU_PARAMETRIC_SURFACE", 
+        .value("PARAMETRIC_SURFACE",
             xms::XmUGridCellType::XMU_PARAMETRIC_SURFACE)
-        .value("XMU_PARAMETRIC_TRI_SURFACE", 
+        .value("PARAMETRIC_TRI_SURFACE",
             xms::XmUGridCellType::XMU_PARAMETRIC_TRI_SURFACE)
-        .value("XMU_PARAMETRIC_QUAD_SURFACE", 
+        .value("PARAMETRIC_QUAD_SURFACE",
             xms::XmUGridCellType::XMU_PARAMETRIC_QUAD_SURFACE)
-        .value("XMU_PARAMETRIC_TETRA_REGION", 
+        .value("PARAMETRIC_TETRA_REGION",
             xms::XmUGridCellType::XMU_PARAMETRIC_TETRA_REGION)
-        .value("XMU_PARAMETRIC_HEX_REGION", 
+        .value("PARAMETRIC_HEX_REGION",
             xms::XmUGridCellType::XMU_PARAMETRIC_HEX_REGION)
-        .value("XMU_HIGHER_ORDER_EDGE", 
+        .value("HIGHER_ORDER_EDGE",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_EDGE)
-        .value("XMU_HIGHER_ORDER_TRIANGLE", 
+        .value("HIGHER_ORDER_TRIANGLE",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_TRIANGLE)
-        .value("XMU_HIGHER_ORDER_QUAD", 
+        .value("HIGHER_ORDER_QUAD",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_QUAD)
-        .value("XMU_HIGHER_ORDER_POLYGON", 
+        .value("HIGHER_ORDER_POLYGON",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_POLYGON)
-        .value("XMU_HIGHER_ORDER_TETRAHEDRON", 
+        .value("HIGHER_ORDER_TETRAHEDRON",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_TETRAHEDRON)
-        .value("XMU_HIGHER_ORDER_WEDGE", 
+        .value("HIGHER_ORDER_WEDGE",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_WEDGE)
-        .value("XMU_HIGHER_ORDER_PYRAMID", 
+        .value("HIGHER_ORDER_PYRAMID",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_PYRAMID)
-        .value("XMU_HIGHER_ORDER_HEXAHEDRON", 
+        .value("HIGHER_ORDER_HEXAHEDRON",
             xms::XmUGridCellType::XMU_HIGHER_ORDER_HEXAHEDRON)
-        .value("XMU_NUMBER_OF_CELL_TYPES", 
+        .value("NUMBER_OF_CELL_TYPES",
             xms::XmUGridCellType::XMU_NUMBER_OF_CELL_TYPES)
         .export_values();
 
     // UGrid FaceOrientation
-    py::enum_<xms::XmUGridFaceOrientation>(xmUg, "xmugrid_faceorientation_enum",
+    py::enum_<xms::XmUGridFaceOrientation>(xmUg, "ugrid_faceorientation_enum",
                             "xmugrid_faceorientation_enum for XmUGrid class")
-        .value("XMU_ORIENTATION_UNKNOWN", 
+        .value("ORIENTATION_UNKNOWN",
             xms::XmUGridFaceOrientation::XMU_ORIENTATION_UNKNOWN)
-        .value("XMU_ORIENTATION_SIDE", 
+        .value("ORIENTATION_SIDE",
             xms::XmUGridFaceOrientation::XMU_ORIENTATION_SIDE)
-        .value("XMU_ORIENTATION_TOP", 
+        .value("ORIENTATION_TOP",
             xms::XmUGridFaceOrientation::XMU_ORIENTATION_TOP)
-        .value("XMU_ORIENTATION_BOTTOM", 
+        .value("ORIENTATION_BOTTOM",
             xms::XmUGridFaceOrientation::XMU_ORIENTATION_BOTTOM)
         .export_values();
 }
