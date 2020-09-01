0e91ff9cc0ccac1bc3fd9db2c22088a56c674a23
Thomas Christiansen <tchristiansen@aquaveo.local>
2020-09-01 10:32:40 -0600

    Migrated geoms code from xmsconstraint to xmsgrid.

--- xmsgrid/geometry/geoms.h
+++ xmsgrid/geometry/geoms.h
@@ -135,10 +135,21 @@ double gm2DDistanceToLineWithTol(const Pt3d* a_pt1,
                                  double a_x,
                                  double a_y,
                                  double a_tol);
+double gm2DDistanceToLineSegment(const Pt3d&, const Pt3d&, const Pt3d&);
+double gm2DDistanceToLineSegmentWithTol(const Pt3d* pt1,
+                                        const Pt3d* pt2,
+                                        double x,
+                                        double y,
+                                        double tol);
 
 double gmXyDistance(const Pt3d& pt1, const Pt3d& pt2);
 double gmXyDistance(double x1, double y1, double x2, double y2);
 double gmXyDistanceSquared(const Pt3d& pt1, const Pt3d& pt2);
+
+double gmDistanceFromPointToPlane(const Pt3d& pt1,
+                                  const Pt3d& pt2,
+                                  const Pt3d& pt3,
+                                  const Pt3d& thePoint);
 
 
 // Intersection
@@ -204,17 +215,98 @@ bool gmPointInOrOnBox2d(const Pt3d& a_bMin, const Pt3d& a_bMax, const Pt3d& a_pt
 
 
 // Finding points on lines
+double gmPtDistanceAlongSegment(const Pt3d& a_pt1,
+                                const Pt3d& a_pt2,
+                                const Pt3d& a_pt,
+                                const double a_tol);
 double gmFindClosestPtOnSegment(const Pt3d& a_pt1,
                                 const Pt3d& a_pt2,
                                 const Pt3d& a_pt,
                                 Pt3d& a_newpt,
                                 const double a_tol);
+//------------------------------------------------------------------------------
+/// \brief Find the closest point on a segment to another point.
+/// \param[in] pt1: First endpoint of segment.
+/// \param[in] pt2: Second endpoint of segment.
+/// \param[in] pt: Test point.
+/// \param[out] newpt: Initialized to the point on the segment that is
+///                    closest to the test point.
+/// \param[in] tol: Tolerance.
+/// \return Parametric value along the line a_pt1, a_pt2 that is the location
+///         of a_newpt.
+//------------------------------------------------------------------------------
+template <typename T>
+double gmFindClosestPtOnSegment(const T& pt1,
+                                const T& pt2,
+                                const T& pt,
+                                T& newpt,
+                                const double* tol = nullptr)
+{
+  double dx(0.0), dy(0.0);
 
-double gmPtDistanceAlongSegment(const Pt3d& a_pt1,
-                                const Pt3d& a_pt2,
-                                const Pt3d& a_pt,
-                                const double a_tol);
+  const double t = gmPtDistanceAlongSegment(pt1, pt2, pt, tol, &dx, &dy);
+  if (t < 0.0)
+  {
+    newpt.x = pt1.x;
+    newpt.y = pt1.y;
+  }
+  else if (t > 1.0)
+  {
+    newpt.x = pt2.x;
+    newpt.y = pt2.y;
+  }
+  else
+  {
+    newpt.x = pt1.x + dx * t;
+    newpt.y = pt1.y + dy * t;
+  }
+  return t;
+} // gmFindClosestPtOnSegment
+//------------------------------------------------------------------------------
+/// \brief Find the distance along a segment to a given point. Distance is a
+///        proportion of the segment's length, from 0 to 1.
+/// \param pt1: The first endpoint of the segment.
+/// \param pt2: The second endpoint of the segment.
+/// \param pt: The test point.
+/// \param a_dx: Initialized to the horizontal length of the line.
+/// \param a_dy: Initialized to the vertical length of the line.
+/// \return The distance along the segment to the point. May be <0 or >1 if the
+///         test point is not on the line.
+//------------------------------------------------------------------------------
+template <typename T>
+double gmPtDistanceAlongSegment(const T& pt1,
+                                const T& pt2,
+                                const T& pt,
+                                const double* tol = NULL,
+                                double* a_dx = NULL,
+                                double* a_dy = NULL)
+{
+  double dx, dy, t;
 
+  dx = pt2.x - pt1.x;
+  dy = pt2.y - pt1.y;
+
+  if ((tol == NULL && (dx == 0.0 && dy == 0.0)) ||
+      (tol != NULL && (sqrt(dx * dx + dy * dy) <= *tol)))
+  {
+    t = -1.0;
+  }
+  else
+  {
+    t = ((pt.x - pt1.x) * dx + (pt.y - pt1.y) * dy) / (dx * dx + dy * dy);
+  }
+
+  if (a_dx)
+  {
+    *a_dx = dx;
+  }
+  if (a_dy)
+  {
+    *a_dy = dy;
+  }
+
+  return t;
+} // gmPtDistanceAlongSegment
 
 // Tolerance
 double gmComputeXyTol(const Pt3d& a_mn, const Pt3d& a_mx);
@@ -267,6 +359,8 @@ Pt3d gmComputePolygonCentroid(const VecPt3d& pts);
 void gmGetConvexHull(const VecPt3d& a_pts, VecPt3d& a_hull, bool a_includeOn = false);
 
 double gmPolygonArea(const Pt3d* pts, size_t npoints);
+
+void gmUnitVector2DPerp(const Pt3d& p1, const Pt3d& p2, Pt3d* v);
 
 
 // Utility functions
