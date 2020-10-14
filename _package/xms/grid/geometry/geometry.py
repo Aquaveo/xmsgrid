49951431abc7f384ccf2f4073d3d4c2aaf5de936
Andrew Creer <acreer@aquaveo.com>
2020-10-14 12:02:04 -0600

    Moved geometry functions into xmsgrid from xmsconstraint.

---
+++ _package/xms/grid/geometry/geometry.py
@@ -0,0 +1,286 @@
+from .. import _xmsgrid
+
+geometry = _xmsgrid.geometry
+
+
+def point_in_polygon_2d(polygon, point):
+    """
+    Check whether a point is inside, outside, or on a polygon.
+    *DON'T* repeat the first point at the end of the polygon list.
+    Points are represented as (x, y, z) triples.
+    Args:
+        polygon: A list of points describing the polygon.
+                 Can be in clockwise or counterclockwise order.
+        point: The point to test.
+    Returns:
+         1 if the point is inside the polygon.
+         0 if the point is on the polygon.
+         -1 if the point is outside the polygon.
+    """
+    return geometry.gmPointInPolygon2D(polygon, point)
+
+
+def polygon_area_2d(polygon):
+    """
+    Compute the 2d planview projection of area of polygon.
+    The last point should not be repeated.
+    Points are represented as (x, y, z) triples.
+    Args:
+        polygon: A list of points defining the polygon.
+                 CCW order gives positive areas, CW gives negative.
+                 The last point should not be repeated.
+    Returns:
+         The area of the polygon.
+    """
+    return geometry.gmPolygonArea(polygon)
+
+
+def get_tol_2d():
+    """
+    Get the global tolerance for 2d float operations.
+    Value may not be meaningful if it has not been set yet.
+    """
+    return geometry.gmGetXyTol()
+
+
+def set_tol_2d(value=1e-9):
+    """
+    Set the global tolerance for 2d float operations.
+    Args:
+        value: The value to set the tolerance to.
+    """
+    geometry.gmSetXyTol(value)
+
+
+def on_line_2d(p1, p2, pt, tol=None):
+    """
+    Determines whether a point pt is on the line defined by p1 and p2.
+    If p1 and p2 are equal within tol, this function may fail.
+    Points are represented as (x, y, z) triples.
+    Args:
+        p1: First point defining a line.
+        p2: Second point defining a line.
+        pt: The point to test.
+        tol: Tolerance for geometric comparison. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        True if the point is on the line passing through p1 and p2 within tolerance.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmOnLineWithTol(p1, p2, pt, tol)
+
+
+def inside_or_on_line_2d(p1, p2, reference_point, pt, tol=None):
+    """
+    Tests whether a point is on the same side of a line as a reference point, or on the line itself.
+    Assumes reference_point is not on the line (within tol).
+    Points are represented as (x, y, z) triples.
+    Args:
+        p1: The first point defining the line.
+        p2: The second point defining the line.
+        reference_point: A point on one side of the line.
+        pt: Point to test.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        A tuple (result, distance).
+        result is True if pt is either on the line or on the same side as reference_point, or False otherwise.
+        distance is how far away from the line pt is. It will be negative if pt is on the same side of the line as
+        reference_point, or positive if pt is on the opposite side of the line.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmInsideOrOnLineWithTol(p1, p2, reference_point, pt, tol)
+
+
+def find_closest_pt_on_segment_2d(pt1, pt2, pt, tol=None):
+    """
+    Finds the closest point to another point on a line segment.
+    Points are represented as (x, y, z) triples.
+    Args:
+        pt1: First point defining the line segment.
+        pt2: Second point defining the line segment.
+        pt: Point used to find closest point on the segment.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        A tuple of (value, point).
+        value is the parametric value along the line segment that is the location of point.
+        point is an (x, y, z) tuple describing the closest point on the line segment to the parameter pt.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmFindClosestPtOnSegment(pt1, pt2, pt, tol)
+
+
+def pt_distance_along_segment_2d(pt1, pt2, pt, tol=None):
+    """
+    Finds the distance along a segment for the location closest to pt.
+    Points are represented as (x, y, z) triples.
+    Args:
+        pt1: First point defining the line segment.
+        pt2: Second point defining the line segment.
+        pt: Point used to find the closest point on the segment.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        Parametric value along the line segment that is the closest to pt.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmPtDistanceAlongSegment(pt1, pt2, pt, tol)
+
+
+def inside_of_line_2d(pt1, pt2, reference_point, pt, tol=None):
+    """
+   Tests whether a point is on the same side of a line as another point.
+   Assumes reference_point is not on the line (within tol).
+    Points are represented as (x, y, z) triples.
+       Args:
+           pt1: The first point defining the line.
+           pt2: The second point defining the line.
+           reference_point: A point on one side of the line.
+           pt: Point to test.
+           tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+       Returns:
+           True if pt is on the same side of the line segment as the reference point, else False.
+       """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmInsideOfLineWithTol(pt1, pt2, reference_point, pt, tol)
+
+
+def lines_intersect_2d(line1_pt1, line1_pt2, line2_pt1, line2_pt2):
+    """
+    Tests whether two line segments intersect.
+    Points are represented as (x, y, z) triples.
+    Args:
+        line1_pt1: First point defining the first line segment.
+        line1_pt2: Second point defining the first line segment.
+        line2_pt1: First point defining the second line segment.
+        line2_pt2: Second point defining the second line segment.
+    Returns:
+        True if the segments intersect, else False.
+    """
+    return geometry.gmLinesIntersect(line1_pt1, line1_pt2, line2_pt1, line2_pt2)
+
+
+def equal_points_2d(p1, p2, tol=None):
+    """
+    Tests whether two points are equal to within tolerance.
+    Points are represented as (x, y, z) triples.
+    Args:
+        p1: The first point.
+        p2: The second point.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        True if the points are equal to within tolerance, else False.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmEqualPointsXY(p1, p2, tol)
+
+
+def on_line_and_between_endpoints_2d(pt1, pt2, pt, tol=None):
+    """
+    Tests whether pt is on the line segment defined by pt1 and pt2.
+    Points are represented as (x, y, z) triples.
+    Args:
+        pt1: First point defining the line segment.
+        pt2: Second point defining the line segment.
+        pt: the point to test.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        True if the point is on the line segment, else False.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gmOnLineAndBetweenEndpointsWithTol(pt1, pt2, pt, tol)
+
+
+def distance_to_line_segment_2d(pt1, pt2, pt, tol=None):
+    """
+    Computes the minimum distance between a point and a line segment.
+    The closest point may be an endpoint. Checks to the tolerance passed in.
+    Points are represented as (x, y, z) triples.
+    Args:
+        pt1: First point defining the line segment.
+        pt2: Second point defining the line segment.
+        pt: The point to find the distance to.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        The minimum distance between pt and the line.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gm2DDistanceToLineSegmentWithTol(pt1, pt2, pt, tol)
+
+
+def cross_2d(origin, a, b):
+    """
+    Computes the cross product of two points.
+    Points are represented as (x, y, z) triples.
+    Args:
+        origin: The origin that the points are relative to.
+        a: The first point to compute the cross product of.
+        b: The second point to compute the cross product of.
+    Returns:
+        The cross product of the two points.
+    """
+    return geometry.gmCross2D(origin, a, b)
+
+
+def distance_2d(pt1, pt2):
+    """
+    Compute the 2D distance between two 3D points.
+    Points are represented as (x, y, z) triples.
+    Args:
+        pt1: The first point.
+        pt2: The second point.
+    Returns:
+        The distance between the two points.
+    """
+    return geometry.gmXyDistance(pt1, pt2)
+
+
+def compute_polygon_centroid_2d(polygon):
+    """
+    Computes the plan view centroid of a non-self-intersecting polygon.
+    Points are represented as (x, y, z) triples.
+    Args:
+        polygon: A list of points defining the polygon.
+    Returns:
+        The point at the polygon's centroid.
+    """
+    return geometry.gmComputePolygonCentroid(polygon)
+
+
+def distance_to_line_2d(pt1, pt2, pt, tol=None):
+    """
+    Get the closest distance from pt to the line defined by pt1 and pt2.
+    Unlike distance_to_line_segment_2d, points outside the line segment
+    defined by pt1 and pt2 will be considered when finding the closest
+    distance to pt.
+    Points are represented as (x, y, z) triples.
+    Args:
+        pt1: First point defining the line.
+        pt2: Second point defining the line.
+        pt: Point to find the distance for.
+        tol: Tolerance. If omitted, uses global tolerance according to get_tol_2d().
+    Returns:
+        The minimum distance between pt and the line.
+    """
+    if tol is None:
+        tol = get_tol_2d()
+    return geometry.gm2DDistanceToLineWithTol(pt1, pt2, pt, tol)
+
+
+def angle_between_edges_2d(endpoint1, common_point, endpoint2):
+    """
+    Compute the CCW angle (0 to 2pi) between two edges which share a point.
+    Points are represented as (x, y, z) triples.
+    Args:
+        endpoint1: The un-shared endpoint of the first edge.
+        common_point: The shared endpoint of the two edges.
+        endpoint2: The un-shared endpoint of the second edge.
+    Returns:
+        The angle between the two edges.
+    """
+    return geometry.gmAngleBetweenEdges(endpoint1, common_point, endpoint2)
--- xmsgrid/python/geometry/geometry_py.cpp
+++ xmsgrid/python/geometry/geometry_py.cpp
@@ -14,6 +14,204 @@ namespace py = pybind11;
 
 //----- Python Interface -------------------------------------------------------
 
-void initGeometry(py::module &m) {
+void initGeometry(py::module &m)
+{
+    // ---------------------------------------------------------------------------
+	// function: gmPointInPolygon2D
+	// ---------------------------------------------------------------------------
+	geometry.def("gmPointInPolygon2D",
+		[](py::iterable& a_polygon, py::iterable& a_point)-> int
+	{
+		BSHP<xms::VecPt3d> polygon = xms::VecPt3dFromPyIter(a_polygon);
+		xms::Pt3d point = xms::Pt3dFromPyIter(a_point);
+		return xms::gmPointInPolygon2D(*polygon, point);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmPolygonArea
+	// ---------------------------------------------------------------------------
+	geometry.def("gmPolygonArea",
+		[](py::iterable& a_polygon)-> double
+	{
+		BSHP<xms::VecPt3d> polygon = xms::VecPt3dFromPyIter(a_polygon);
+		return xms::gmPolygonArea((*polygon).data(), (*polygon).size());
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmGetXyTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gmGetXyTol",
+		[]()-> double
+	{
+		return xms::gmXyTol();
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmSetXyTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gmSetXyTol",
+		[](double a_value)-> void
+	{
+		xms::gmXyTol(true, a_value);
+	}, py::arg("a_value")=1e-9);
+	// ---------------------------------------------------------------------------
+	// function: gmOnLineWithTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gmOnLineWithTol",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_pt, const double a_tol)-> bool
+	{
+		xms::Pt3d pt1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d pt2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		return xms::gmOnLineWithTol(pt1, pt2, pt.x, pt.y, a_tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmInsideOrOnLineWithTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gmInsideOrOnLineWithTol",
+		[](const py::iterable& a_p1, const py::iterable& a_p2, const py::iterable& a_referencePoint,
+		   const py::iterable& a_pt, double a_tol)-> py::tuple
+	{
+		xms::Pt3d p1 = xms::Pt3dFromPyIter(a_p1);
+		xms::Pt3d p2 = xms::Pt3dFromPyIter(a_p2);
+		xms::Pt3d referencePoint = xms::Pt3dFromPyIter(a_referencePoint);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		double dist;
+		bool result = xms::gmInsideOrOnLineWithTol(&p1, &p2, &referencePoint, pt.x, pt.y, a_tol, &dist);
+		return py::make_tuple(result, dist);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmFindClosestPtOnSegment
+	// ---------------------------------------------------------------------------
+	geometry.def("gmFindClosestPtOnSegment",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_pt,
+		   const double a_tol)-> py::tuple
+	{
+		xms::Pt3d p1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d p2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		xms::Pt3d newPoint;
+		double value = xms::gmFindClosestPtOnSegment(p1, p2, pt, newPoint, a_tol);
+		return py::make_tuple(value, py::make_tuple(newPoint.x, newPoint.y, newPoint.z));
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmPtDistanceAlongSegment
+	// ---------------------------------------------------------------------------
+	geometry.def("gmPtDistanceAlongSegment",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_pt, const double tol)-> double
+	{
+		xms::Pt3d p1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d p2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		return xms::gmPtDistanceAlongSegment(p1, p2, pt, tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmInsideOfLineWithTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gmInsideOfLineWithTol",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_referencePoint,
+		   const py::iterable& a_pt, const double a_tol)-> bool
+	{
+		xms::Pt3d pt1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d pt2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d reference = xms::Pt3dFromPyIter(a_referencePoint);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		return xms::gmInsideOfLineWithTol(pt1, pt2, reference, pt.x, pt.y, a_tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmLinesIntersect
+	// ---------------------------------------------------------------------------
+	geometry.def("gmLinesIntersect",
+		[](const py::iterable& a_line1pt1, const py::iterable& a_line1pt2, const py::iterable& a_line2pt1,
+		   const py::iterable& a_line2pt2)-> bool
+	{
+		xms::Pt3d line1pt1 = xms::Pt3dFromPyIter(a_line1pt1);
+		xms::Pt3d line1pt2 = xms::Pt3dFromPyIter(a_line1pt2);
+		xms::Pt3d line2pt1 = xms::Pt3dFromPyIter(a_line2pt1);
+		xms::Pt3d line2pt2 = xms::Pt3dFromPyIter(a_line2pt2);
+		return xms::gmLinesIntersect(line1pt1, line1pt2, line2pt1, line2pt2);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmEqualPointsXY
+	// ---------------------------------------------------------------------------
+	geometry.def("gmEqualPointsXY",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, double tol)-> bool
+	{
+		xms::Pt3d p1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d p2 = xms::Pt3dFromPyIter(a_pt2);
+		return xms::gmEqualPointsXY(p1.x, p1.y, p2.x, p2.y, tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmOnLineAndBetweenEndpointsWithTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gmOnLineAndBetweenEndpointsWithTol",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_pt, double a_tol)-> bool
+	{
+		xms::Pt3d pt1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d pt2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		return xms::gmOnLineAndBetweenEndpointsWithTol(pt1, pt2, pt.x, pt.y, a_tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gm2DDistanceToLineSegmentWithTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gm2DDistanceToLineSegmentWithTol",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_pt, double a_tol)-> double
+	{
+		xms::Pt3d pt1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d pt2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d pt = xms::Pt3dFromPyIter(a_pt);
+		return xms::gm2DDistanceToLineSegmentWithTol(&pt1, &pt2, pt.x, pt.y, a_tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmCross2D
+	// ---------------------------------------------------------------------------
+	geometry.def("gmCross2D",
+		[](const py::iterable& a_origin, const py::iterable& a_a, const py::iterable& a_b)-> double
+	{
+		xms::Pt3d origin = xms::Pt3dFromPyIter(a_origin);
+		xms::Pt3d a = xms::Pt3dFromPyIter(a_a);
+		xms::Pt3d b = xms::Pt3dFromPyIter(a_b);
+		return xms::gmCross2D(origin, a, b);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmXyDistance
+	// ---------------------------------------------------------------------------
+	geometry.def("gmXyDistance",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2)
+	{
+		xms::Pt3d pt1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d pt2 = xms::Pt3dFromPyIter(a_pt2);
+		return xms::gmXyDistance(pt1, pt2);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmComputePolygonCentroid
+	// ---------------------------------------------------------------------------
+	geometry.def("gmComputePolygonCentroid",
+		[](const py::iterable& a_polygon)-> py::tuple
+	{
+		BSHP<xms::VecPt3d> polygon = xms::VecPt3dFromPyIter(a_polygon);
+		xms::Pt3d result = xms::gmComputePolygonCentroid(*polygon);
+		return xms::PyIterFromPt3d(result);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gm2DDistanceToLineWithTol
+	// ---------------------------------------------------------------------------
+	geometry.def("gm2DDistanceToLineWithTol",
+		[](const py::iterable& a_pt1, const py::iterable& a_pt2, const py::iterable& a_pt3, double a_tol)-> double
+	{
+		xms::Pt3d pt1 = xms::Pt3dFromPyIter(a_pt1);
+		xms::Pt3d pt2 = xms::Pt3dFromPyIter(a_pt2);
+		xms::Pt3d pt3 = xms::Pt3dFromPyIter(a_pt3);
+		return xms::gm2DDistanceToLineWithTol(&pt1, &pt2, pt3.x, pt3.y, a_tol);
+	});
+	// ---------------------------------------------------------------------------
+	// function: gmAngleBetweenEdges
+	// ---------------------------------------------------------------------------
+	geometry.def("gmAngleBetweenEdges",
+		[](const py::iterable& a_endpoint1, const py::iterable& a_commonPoint, const py::iterable& a_endpoint2)-> double
+	{
+		xms::Pt3d endpoint1 = xms::Pt3dFromPyIter(a_endpoint1);
+		xms::Pt3d commonPoint = xms::Pt3dFromPyIter(a_commonPoint);
+		xms::Pt3d endpoint2 = xms::Pt3dFromPyIter(a_endpoint2);
+		return xms::gmAngleBetweenEdges(endpoint1, commonPoint, endpoint2);
+	});
     initGmTriSearch(m);
 }
