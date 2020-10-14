17e3530c1264da15d02d553b88c9b48d577fd028
Andrew Creer <acreer@aquaveo.com>
2020-10-14 12:30:57 -0600

    Update copyright.

--- xmsgrid/python/geometry/geometry_py.cpp
+++ xmsgrid/python/geometry/geometry_py.cpp
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 /// \file
 /// \brief
-/// \copyright (C) Copyright Aquaveo 2018. Distributed under FreeBSD License
+/// \copyright (C) Copyright Aquaveo 2020. Distributed under FreeBSD License
 /// (See accompanying file LICENSE or https://aqaveo.com/bsd/license.txt)
 //------------------------------------------------------------------------------
 
