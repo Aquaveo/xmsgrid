c92d7871bbf5f0752e7bf69e93e3011bf56cca4f
aclark <aclark@aquaveo.com>
2020-11-11 16:23:14 -0700

    Update xmscore version.

--- _package/setup.py
+++ _package/setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 requires = [
     'numpy',
-    'xmscore>=4.0.0',
+    'xmscore>=4.0.1',
 ]
 
 version = __version__
