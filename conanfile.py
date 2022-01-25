5489d1f4e06af8e952ce60a6a1ffa4425076ee8e
Thomas Christiansen <tchristiansen@aquaveo.local>
2022-01-25 15:48:31 -0700

    Update xmscore version used.

--- conanfile.py
+++ conanfile.py
@@ -62,7 +62,7 @@ class XmsgridConan(ConanFile):
         self.requires("boost/1.74.0.3@aquaveo/stable")
         if self.options.pybind:
             self.requires("pybind11/2.5.0@aquaveo/testing")
-        self.requires("xmscore/4.0.2@aquaveo/stable")
+        self.requires("xmscore/5.0.1@aquaveo/stable")
 
     def build(self):
         cmake = CMake(self)
