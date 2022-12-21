4c8f5955a37ba6863c91880e25593066b2452bc7
Bill Dolinar <bill_dolinar@yahoo.com>
2022-12-21 09:55:13 -0700

    Fix tests to work with numpy 1.24.0.

--- _package/tests/unit_tests/triangulate_tests/tin_pyt.py
+++ _package/tests/unit_tests/triangulate_tests/tin_pyt.py
@@ -16,8 +16,8 @@ class TestTrTin(unittest.TestCase):
         self.pts_np = np.array([(0, 0, 0), (10, 0, 0), (20, 0, 0), (5, 10, 0), (15, 10, 0)])
         self.tris = (3, 0, 1, 1, 2, 4, 1, 4, 3)
         self.tris_np = np.array([3, 0, 1, 1, 2, 4, 1, 4, 3])
-        self.tris_adj = ((0,), (0, 1, 2), (1,), (0, 2), (1, 2))
-        self.tris_adj_np = np.array([[0], [0, 1, 2], [1], [0, 2], [1, 2]], dtype=object)
+        self.tris_adj_tuple = ((0,), (0, 1, 2), (1,), (0, 2), (1, 2))
+        self.tris_adj_list = [[0], [0, 1, 2], [1], [0, 2], [1, 2]]
         self.Tin = xt.Tin(self.pts, self.tris)
 
     def test_set_pts(self):
@@ -52,48 +52,40 @@ class TestTrTin(unittest.TestCase):
     def test_set_tris_adj(self):
         """Test setting the triangles adjacent to points of a tin."""
         trtin = xt.Tin(self.pts)
-        tris_adj = self.tris_adj
+        tris_adj = self.tris_adj_tuple
         trtin.triangles_adjacent_to_points = tris_adj
-        expected = np.array(tris_adj, dtype=object)
-        actual = np.array(trtin.triangles_adjacent_to_points, dtype=object)
-        np.testing.assert_array_equal(expected, actual)
+        self.assertEqual(self.tris_adj_tuple, trtin.triangles_adjacent_to_points)
 
-    def test_set_tris_adj_numpy(self):
-        """Test setting the triangles adjacent to points of a tin using numpy arrays."""
+    def test_set_tris_adj_list(self):
+        """Test setting the triangles adjacent to points of a tin using a list."""
         trtin = xt.Tin(self.pts)
-        tris_adj = self.tris_adj_np
+        tris_adj = self.tris_adj_list
         trtin.triangles_adjacent_to_points = tris_adj
-        expected = tris_adj
-        actual = np.array(trtin.triangles_adjacent_to_points, dtype=object)
-        np.testing.assert_array_equal(expected, actual)
+        self.assertEqual(self.tris_adj_tuple, trtin.triangles_adjacent_to_points)
 
     def test_set_geometry(self):
         """Test setting the geometry of a tin."""
         trtin = xt.Tin(self.pts)
         pts = self.pts
         tris = self.tris
-        tris_adj = self.tris_adj
+        tris_adj = self.tris_adj_tuple
         trtin.set_geometry(pts, tris, tris_adj)
 
         np.testing.assert_array_equal(pts, trtin.points)
         np.testing.assert_array_equal(tris, trtin.triangles)
-        expected = tris_adj
-        actual = np.array(trtin.triangles_adjacent_to_points, dtype=object)
-        np.testing.assert_array_equal(expected, actual)
+        self.assertEqual(self.tris_adj_tuple, trtin.triangles_adjacent_to_points)
 
     def test_set_geometry_numpy(self):
         """Test setting the geometry of a tin using numpy arrays."""
         trtin = xt.Tin(self.pts)
         pts = self.pts_np
         tris = self.tris_np
-        tris_adj = self.tris_adj_np
+        tris_adj = np.array(self.tris_adj_list, dtype=object)
         trtin.set_geometry(pts, tris, tris_adj)
 
         np.testing.assert_array_equal(pts, trtin.points)
         np.testing.assert_array_equal(tris, trtin.triangles)
-        expected = tris_adj
-        actual = np.array(trtin.triangles_adjacent_to_points, dtype=object)
-        np.testing.assert_array_equal(expected, actual)
+        self.assertEqual(self.tris_adj_tuple, trtin.triangles_adjacent_to_points)
 
     def test_get_pts(self):
         """Test getting the points of a tin."""
@@ -112,11 +104,9 @@ class TestTrTin(unittest.TestCase):
     def test_get_tris_adj(self):
         """Test getting the triangles adjacent to points."""
         trtin = xt.Tin(self.pts)
-        tris_adj = self.tris_adj
+        tris_adj = self.tris_adj_tuple
         trtin.triangles_adjacent_to_points = tris_adj
-        expected = np.array(tris_adj, dtype=object)
-        actual = np.array(trtin.triangles_adjacent_to_points, dtype=object)
-        np.testing.assert_array_equal(expected, actual)
+        self.assertEqual(self.tris_adj_tuple, trtin.triangles_adjacent_to_points)
 
     def test_num_points(self):
         """Test getting the number of points in a tin."""
@@ -135,7 +125,7 @@ class TestTrTin(unittest.TestCase):
     def test_triangle_from_edge(self):
         """Test getting a triangle from an edge."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         rv, tri, idx1, idx2 = trtin.triangle_from_edge(1, 3)
         self.assertEqual(rv, True)
         self.assertEqual(tri, 0)
@@ -152,7 +142,7 @@ class TestTrTin(unittest.TestCase):
     def test_triangle_adjacent_to_edge(self):
         """Test getting the triangle adjacent to an edge."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         tri = trtin.triangle_adjacent_to_edge(1, 3)
         self.assertEqual(2, tri)
         tri = trtin.triangle_adjacent_to_edge(3, 1)
@@ -163,7 +153,7 @@ class TestTrTin(unittest.TestCase):
     def test_local_index(self):
         """Test getting local indices."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(1, trtin.local_index(0, 0))
         self.assertEqual(2, trtin.local_index(0, 1))
         self.assertEqual(0, trtin.local_index(0, 3))
@@ -172,7 +162,7 @@ class TestTrTin(unittest.TestCase):
     def test_global_index(self):
         """Test getting global indices."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(3, trtin.global_index(0, 0))
         self.assertEqual(0, trtin.global_index(0, 1))
         self.assertEqual(1, trtin.global_index(0, 2))
@@ -181,7 +171,7 @@ class TestTrTin(unittest.TestCase):
     def test_common_edge_index(self):
         """Test getting the common edge between two adjacent triangles."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(2, trtin.common_edge_index(0, 2))
         self.assertEqual(2, trtin.common_edge_index(2, 0))
         self.assertEqual(2, trtin.common_edge_index(1, 2))
@@ -191,7 +181,7 @@ class TestTrTin(unittest.TestCase):
     def test_adjacent_triangle(self):
         """Test getting the triangles adjacent to another triangle."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(-1, trtin.adjacent_triangle(0, 0))
         self.assertEqual(-1, trtin.adjacent_triangle(0, 1))
         self.assertEqual(2, trtin.adjacent_triangle(0, 2))
@@ -205,7 +195,7 @@ class TestTrTin(unittest.TestCase):
     def test_triangle_centroid(self):
         """Test getting the centroid of a triangle."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         x0, y0, z0 = trtin.triangle_centroid(0)
         self.assertEqual(5, x0)
         self.assertAlmostEqual(3.333333333, y0, places=8)
@@ -222,7 +212,7 @@ class TestTrTin(unittest.TestCase):
     def test_triangle_area(self):
         """Test getting the area of a triangle."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertAlmostEqual(50.00000, trtin.triangle_area(0), places=4)
         self.assertAlmostEqual(50.00000, trtin.triangle_area(1), places=4)
         self.assertAlmostEqual(50.00000, trtin.triangle_area(2), places=4)
@@ -230,7 +220,7 @@ class TestTrTin(unittest.TestCase):
     def test_next_boundry_point(self):
         """Test getting the boundary point after another boundary point."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(3, trtin.next_boundary_point(0))
         self.assertEqual(4, trtin.next_boundary_point(3))
         self.assertEqual(2, trtin.next_boundary_point(4))
@@ -240,7 +230,7 @@ class TestTrTin(unittest.TestCase):
     def test_previous_boundary_point(self):
         """Test getting the boundary point previous to another boundary point."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(1, trtin.previous_boundary_point(0))
         self.assertEqual(2, trtin.previous_boundary_point(1))
         self.assertEqual(4, trtin.previous_boundary_point(2))
@@ -265,13 +255,13 @@ class TestTrTin(unittest.TestCase):
     def test_get_boundary_polys(self):
         """Test getting the boundary polygons of a tin."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         self.assertEqual(((0, 3, 4, 2, 1, 0),), trtin.boundary_polys)
 
     def test_get_extents(self):
         """Test getting the extents of a tin."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         mn, mx = trtin.extents
         self.assertEqual((0, 0, 0), mn)
         self.assertEqual((20, 10, 0), mx)
@@ -279,14 +269,14 @@ class TestTrTin(unittest.TestCase):
     def test_export_tin_file(self):
         """Test exporting a tin to an ASCII file."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         trtin.export_tin_file("testfile.txt")
         self.assertTrue(os.path.isfile("testfile.txt"))
 
     def test_swap_edge(self):
         """Test swapping edges of a tin."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         rv = trtin.swap_edge(0, 2, True)
         self.assertEqual(True, rv)
         self.assertEqual(trtin.triangles[0], 3)
@@ -310,7 +300,7 @@ class TestTrTin(unittest.TestCase):
         """Test deleting triangles from a tin."""
         # TODO: I don't think this is right.
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         to_delete = (1, 0)
         trtin.delete_triangles(to_delete)
         self.assertEqual(5, trtin.number_of_points)
@@ -319,7 +309,7 @@ class TestTrTin(unittest.TestCase):
     def test_delete_points(self):
         """Test deleting points from a tin."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         to_delete = (2, 4)
         trtin.delete_points(to_delete)
         self.assertEqual(3, trtin.number_of_points)
@@ -341,17 +331,17 @@ class TestTrTin(unittest.TestCase):
     def test_build_tris_adj_to_pts(self):
         """Test building triangles adjacent to points."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         trtin.build_triangles_adjacent_to_points()
         self.assertEqual(((0,), (0, 1, 2), (1,), (0, 2), (1, 2)), trtin.triangles_adjacent_to_points)
 
     def test_clear(self):
         """Test clearing a tin."""
         trtin = xt.Tin(self.pts)
-        trtin.set_geometry(self.pts, self.tris, self.tris_adj)
+        trtin.set_geometry(self.pts, self.tris, self.tris_adj_tuple)
         np.testing.assert_array_equal(self.pts_np, trtin.points)
         np.testing.assert_array_equal(self.tris_np, trtin.triangles)
-        self.assertEqual(self.tris_adj, trtin.triangles_adjacent_to_points)
+        self.assertEqual(self.tris_adj_tuple, trtin.triangles_adjacent_to_points)
         trtin.clear()
         self.assertEqual(0, len(trtin.points))
         self.assertEqual(0, len(trtin.triangles))
