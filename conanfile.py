35e7e2f7327b3dc1dd83eb6a4af46d97a9dd1b15
Gage Larsen <glarsen@aquaveo.com>
2025-01-22 10:50:00 -0700

    update for conan2

--- conanfile.py
+++ conanfile.py
@@ -6,13 +6,12 @@ from xmsconan.xms_conan2_file import XmsConan2File
 
 class XmsgridConanFile(XmsConan2File):
     """
-    XmsgridConan class used for defining the conan info.
+    XmsgridConanFile class used for defining the conan info.
     """
     name = "xmsgrid"
     url = "https://github.com/Aquaveo/xmsgrid"
     description = "Grid library for XMS products"
     xms_dependencies = [
-        # "xmscore/[>=6.0.1 <7.0.0]",
         "xmscore/6.2.5-5-g2bd6574b",
     ]
     extra_export_sources = ['test_files']
