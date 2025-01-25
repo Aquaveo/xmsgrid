a088ecb54917ef8f3f522ebaf325ab2143e4ccff
Bill Dolinar <bill_dolinar@yahoo.com>
2025-01-25 12:22:08 -0700

    Fixed macOS Python build
    Fixed Python build on macOS.
    Uses latest generator template.

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class XmsgridConanFile(XmsConan2File):
     url = "https://github.com/Aquaveo/xmsgrid"
     description = "Grid library for XMS products"
     xms_dependencies = [
-        "xmscore/6.2.5-6-g72e83110",
+        "xmscore/6.2.5-8-gfabf5914",
     ]
     extra_export_sources = ['test_files']
 
